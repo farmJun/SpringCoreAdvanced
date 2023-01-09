d035f0d78f74a3e83435a70cf7802ffd8f183df7
박준영 <wnsdud6969@naver.com>
2023-01-09 17:34:42 +0900

    feat: 필드 동기화 로그 추적기 구현

---
+++ src/main/java/core/advanced/trace/logTrace/LogTrace.java
@@ -0,0 +1,11 @@
+package core.advanced.trace.logTrace;
+
+import core.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+    TraceStatus begin(String message);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+}
