d28b77d8fa5a5b9fdb68e71f4205b790ea843f10
박준영 <wnsdud6969@naver.com>
2023-01-28 11:50:42 +0900

    test: 템플릿 메서드 패턴 예제 테스트

--- src/test/java/core/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/core/advanced/trace/template/TemplateMethodTest.java
@@ -29,4 +29,27 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
     }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() { log.info("비즈니스 로직1 실행");
+            } };
+        log.info("클래스 이름1={}", template1.getClass()); template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() { log.info("비즈니스 로직1 실행");
+            } };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
 }
