281c7916301708d1fa3832836fe6c360ad7385cd
박준영 <wnsdud6969@naver.com>
2023-02-13 16:59:36 +0900

    test: 람다 전략 패턴 테스트

--- src/test/java/core/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/core/advanced/trace/strategy/ContextV1Test.java
@@ -82,4 +82,13 @@ public class ContextV1Test {
         });
         context2.execute();
     }
+
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
+
 }
