e3305ab1dfbbaa91de8331331f06006863bf4179
박준영 <wnsdud6969@naver.com>
2023-02-13 16:52:36 +0900

    test: 익명 내부 클래스 전략 패턴 테스트

--- src/test/java/core/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/core/advanced/trace/strategy/ContextV1Test.java
@@ -64,4 +64,22 @@ public class ContextV1Test {
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
     }
+
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
 }
