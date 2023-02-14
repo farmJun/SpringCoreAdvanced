6c65c8d44ae8ab1901afc8a648bc29c5a195033b
박준영 <wnsdud6969@naver.com>
2023-02-14 19:30:36 +0900

    test: 파라미터 전략 패턴 테스트

---
+++ src/test/java/core/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,16 @@
+package core.advanced.trace.strategy;
+
+import core.advanced.trace.strategy.code.StrategyLogic1;
+import core.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+}
