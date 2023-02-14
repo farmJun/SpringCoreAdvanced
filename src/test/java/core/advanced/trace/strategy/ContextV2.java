71c8a4a192eb9e06ec8548062cbd8601774f3a9b
박준영 <wnsdud6969@naver.com>
2023-02-14 19:11:36 +0900

    feat: context 추가

---
+++ src/test/java/core/advanced/trace/strategy/ContextV2.java
@@ -0,0 +1,16 @@
+package core.advanced.trace.strategy;
+
+import core.advanced.trace.strategy.code.Strategy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV2 {
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+        strategy.call(); //위임
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
