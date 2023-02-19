4eee6d0b20c3357d48b2662ee2d7af3f91b32822
박준영 <wnsdud6969@naver.com>
2023-02-19 20:10:36 +0900

    test: 익명 내부 클래스 템플릿 콜백 패턴 테스트

---
+++ src/test/java/core/advanced/trace/strategy/TemplateCallbackTest.java
@@ -0,0 +1,26 @@
+package core.advanced.trace.strategy;
+
+import core.advanced.trace.strategy.code.template.Callback;
+import core.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+    @Test
+    void callbackV1() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        template.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+}
